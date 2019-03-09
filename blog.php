7437fd829492deaa1dbdc27e72c10dbad2b882f6
githumbi <githumbi74@gmail.com>
2019-03-09 19:37:31 +0300

    added blog.php

---
+++ blog.php
@@ -0,0 +1,147 @@
+<?php
+$active = 'blog';
+include 'includes/nav.php';
+?>
+
+<div class="content-bottom">
+	 <div class="container">
+		  <div class="wlid-sec">
+			 <div class="col-md-6 news">
+				 <div class="news-head">
+					 <h3>Latest News</h3>
+					 <span></span>
+				 </div>
+				 <div class="news_sec">
+					 <img src="images/new.jpg" class="img-responsive" alt=""/>
+					 <h3><a href="#">Mauris hendrerit ex id leo elementum congue.</a></h3>
+					 <p>Praesent accumsan augue dolor, vel eleifend lorem pellentesque egestas. Ut sit amet iaculis erat. Cras venenatis sodales nibh non finibus. 
+					 Mauris dapibus ex ante. Aenean quis vestibulum mi. Mauris scelerisque urna id lectus suscipit, sed mattis lorem congue. Fusce dapibus tempor
+					 augue. Ut ac pharetra nisl. </p>
+					 <a class="read" href="about.html">Read More...</a>
+				 </div>
+			 </div>
+			 <div class="col-md-6 testimonal">
+				 <div class="testi-head">
+					 <h3>Testimonals</h3>
+					 <span></span>
+				 </div>
+				 <div class="testi-grids">
+					 <div class="testi-grid">
+						 <div class="people">
+							 <img src="images/t1.jpg" class="img-responsive" alt=""/>
+						 </div>
+						 <div class="testi-info">
+							 <h4>John Deo</h4>
+							 <a href="projects.html">Program Director</a>
+							 <p>Nullam viverra porttitor est at vestibulum. Quisque sodales justo eu erat malesuada rutrum. Ut orci velit, dignissim ut dui convallis, sagittis cursus sem.</p>
+						 </div>
+						 <div class="clearfix"></div>
+					 </div>
+					 <div class="testi-grid testi2">
+						 <div class="people">
+							 <img src="images/t2.jpg" class="img-responsive" alt=""/>
+						 </div>
+						 <div class="testi-info">
+							 <h4>John Deo</h4>
+							 <a href="projects.html">Program Director</a>
+							 <p>Nullam viverra porttitor est at vestibulum. Quisque sodales justo eu erat malesuada rutrum. Ut orci velit, dignissim ut dui convallis, sagittis cursus sem.</p>
+						 </div>
+						 <div class="clearfix"></div>
+					 </div>
+				 </div>
+			 </div>
+			 <div class="clearfix"></div>
+		  </div>
+	 </div>
+</div>
+<!----> 
+
+<div class="service">
+	 <div class="container">
+		 <h3>Services</h3>
+		  <div class="works">			  
+			  <div class="prjt-grid">
+				 <div class="box maxheight">
+					  <a class="example-image-link" href="images/s1.jpg" data-lightbox="example-1" data-title="Guidance."><img class="example-image" src="images/s1.jpg"></a>
+					  <div class="project-info">
+					   <a href="about.html">Guidance</a>
+					   <p>Kuspendisse laoreet augue iderti wer  interdum merti oremolo lectusto odio, sedorolu fringilla estero libero orciwe. </p>
+					  </div>
+				 </div>
+			  </div>
+			  <div class="prjt-grid">
+				 <div class="box maxheight">
+					  <a class="example-image-link" href="images/s2.jpg" data-lightbox="example-2" data-title="Mobile Tents."><img class="example-image" src="images/s2.jpg"></a>
+					  <div class="project-info">
+					  <a href="about.html">Mobile Tents</a>
+					  <p>Kuspendisse laoreet augue iderti wer  interdum merti oremolo lectusto odio, sedorolu fringilla estero libero orciwe. </p>
+					  </div>
+				  </div>
+			  </div>
+			  <div class="prjt-grid span66">
+				 <div class="box maxheight">
+					  	<a class="example-image-link" href="images/s3.jpg" data-lightbox="example-3" data-title="Animal Care."><img class="example-image" src="images/s3.jpg"></a>
+					  <div class="project-info">
+					  <a href="about.html">Animal Care</a>
+					  <p>Kuspendisse laoreet augue iderti wer  interdum merti oremolo lectusto odio, sedorolu fringilla estero libero orciwe.</p>
+					  </div>					  
+				  </div>
+			  </div>
+			 <div class="clearfix"></div>
+		 </div>
+	 </div>	
+</div>	
+<script src="js/lightbox-plus-jquery.min.js"></script>	 			 
+<!---->
+
+<div class="footer">
+	 <div class="container">
+		 <div class="footer-sec">
+			 <div class="col-md-4 ftr-grid1">
+				 <h3>Latest Tweets</h3>
+				 <div class="twts">
+					 <h5>Nunc porta justo eu varius mattis.</h5>
+					 <a href="mailto:example@gmail.com">http://www.example.com</a>
+				 </div>
+				 <div class="twts">
+					 <h5>Nunc porta justo eu varius mattis.</h5>
+					 <a href="mailto:example@gmail.com">http://www.example.com</a>
+				 </div>
+			 </div>
+			 <div class="col-md-4 news-ltr">
+				 <h3>NewsLetter</h3>
+				 <p>Praesent ex lectus, luctus ut enim sed, scelerisque laoreet massa. Fusce rhoncus massa dignissim suscipit varius.
+				 Donec vel congue libero. </p>
+				 <form>					 
+					  <input type="text" class="text" value="Enter Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter Email';}">
+					 <input type="submit" value="Go">
+					 <div class="clearfix"></div>
+				 </form>
+			 </div>
+			 <div class="col-md-4 social">
+				 <h3>Social Media</h3>
+				 <a href="#"><i class="facebook"></i></a>
+				 <a href="#"><i class="twitter"></i></a>
+				 <a href="#"><i class="dribble"></i></a>	
+				 <a href="#"><i class="google"></i></a>	
+				 <a href="#"><i class="youtube"></i></a>
+			 </div>
+			 <div class="clearfix"></div>
+     	 </div>
+	 </div>
+</div>
+<!---->  
+<div class="copywrite">
+	 <div class="container">
+		 <div class="ftr-logo">
+			 <h3><a href="index.html">WILD LIFE</a></h3>
+		 </div>
+		 <div class="ftr-right">
+			 <p>Copyright © 2015 Wild Life. All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+		 </div>
+		 <div class="clearfix"></div>
+	 </div>
+</div>
+<!----> 
+</body>
+</html>
